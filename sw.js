778f0c88c4aafac6c2fce351fa9258458459b6b1
vasilis1730-web <vasilis1730@gmail.com>
2026-08-06 14:56:54 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
    network is unavailable.
    ──────────────────────────────────────────────────────────────── */
 
-const CACHE_VERSION = 'rodios-v9-19-3';
+const CACHE_VERSION = 'rodios-v9-19-7';
 
 const STATIC_ASSETS = [
   './icon-192.png',
